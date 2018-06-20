64f89918f24c431c12b2d849e6c0cd7a02eeb6a2
Tomáš Hendrych <hendrych@servodata.net>
2018-06-20 18:36:11 +0200

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,13 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/prettier'
-  ],
+  extends: ['plugin:vue/essential', '@vue/prettier'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'quotes': [2, 'single', 'avoid-escape']
   },
   parserOptions: {
     parser: 'babel-eslint'
   }
-}
+};
